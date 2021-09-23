ee356b0616c9ca18aff8be9dea4428f681d6e7bd
Torugo21 <hakermelgar@gmail.com>
2021-09-23 14:32:35 -0600

    routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren:()=>import('./auth/auth.module').then((m)=>m.AuthModule),
+    },
+    {
+      path:'empleado',
+      loadChildren:()=>import('./empleado/empleado.module').then((m)=>m.EmpleadoModule),
+      },
+      {
+        path:'shared',
+        loadChildren:()=>import('./shared/shared.module').then((m)=>m.SharedModule),
+        }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
